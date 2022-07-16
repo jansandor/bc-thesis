dc9b03a3cbcaf8e661ae02fd92f3aa462de8e4f4
Jan Šándor <jan.sandor@colorcare.cz>
2022-07-16 23:24:21 +0200

    fix: fixed active_nav_link arguments in main_nav.html which pointed to removed views (beneficiaries, contact) causing exception

--- sportdiag/templatetags/nav.py
+++ sportdiag/templatetags/nav.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def active_nav_link(request, view_name):
+def active_nav_link(request, view_name, anchor=''):
     view = resolve(request.path).view_name
-    if view == view_name:
+    if view == view_name + anchor:
         return "active"
     return ""
