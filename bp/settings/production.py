0442d42db7b41f1da848bea13bacb70660d29e90
Jan Šándor <sandor.jan9@gmail.com>
2024-08-26 18:57:17 +0200

    update prod settings to use SQLite

--- bp/settings/production.py
+++ bp/settings/production.py
@@ -43,14 +43,21 @@ EMAIL_SUBJECT_PREFIX = config('EMAIL_SUBJECT_PREFIX_PROD', cast=lambda v: v + " 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
